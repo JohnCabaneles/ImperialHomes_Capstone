15a72d5b0be6d1ca75a57f72dfe4985054a710d1
vincentUgale04 <vincentugale04@gmail.com>
2023-02-28 01:48:17 +0800

    featured 28/02/2023

--- resources/views/components/featured-property.blade.php
+++ resources/views/components/featured-property.blade.php
@@ -1,146 +1,99 @@
-<!-- ######################### Start of Featured Properties ############################### -->
+<!-- #######Start of Featured Properties ############################### -->
 <div class="container my-24 px-6 mx-auto">
 
   <!-- Section: Design Block -->
-  <section class="mb-32 text-gray-800">
+  <section class="mb-32 text-gray-800 text-center">
 
-    <h2 class="title text-4xl font-bold mb-12 pb-4 text-center text-white "><span class="text-amber-500">Featured</span> Properties</h2>
+    <h2 class="title text-4xl font-bold mb-12 pb-4 text-center text-white"><span class="text-amber-500">Featured</span> Properties</h2>
 
-    <div class="grid lg:grid-cols-3 gap-3 xl:gap-x-4 text-left">
-      <section class="card">
-                    <div class="rounded-lg shadow-lg bg-white mx-2 mt-10">
-                        <img class="rounded-t-lg w-full h-60"
-                            src="https://mdbcdn.b-cdn.net/img/new/standard/city/024.webp" alt="" />
-                        <div class="p-6">                            
-                                <h5 class="text-2xl text-amber-500 font-bold mb-2">4BR House & Lot for Sale </h5>                        
-                            <div
-                                class="text-lg font-thin my-4 flex items-center text-white rounded-xl py-1 bg-sky-700 px-3 mr-2">
-                                <i class="fa-solid fa-location-dot text-white mr-2"></i> CATARMAN, LILOAN
-                            </div>
-                            <div class="flex items-center gap-1 mb-2">
-                                <p
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    House
-                                </p>
-                                <p
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    model</p>
-                            </div>
-
-                            <div class="flex items-center gap-1">
-                                <div
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <span>Php </span>
-                                    ₱ 10,314,000
-                                </div>
-                                
-                            </div>
-
-                            <br />
-                            <button type="button"
-                                class="inline-block px-6 py-2.5 border-2 border-sky-600 text-sky-600
-                                              hover:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-700 hover:shadow-lg focus:bg-sky-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-800 active:shadow-lg transition duration-150 ease-in-out">
-                                Button
-                            </button>
-
-                        </div>
-                    </div>
-                </section>
+    <div class="grid lg:grid-cols-3 gap-3 xl:gap-x-4">
+      <div class="mb-6 lg:mb-0 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-10 duration-300">
+        <div class="relative block bg-white rounded-lg shadow-lg h-full">
+          <div class="flex">
+            <div
+              class="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+              data-mdb-ripple="true" data-mdb-ripple-color="light">
+              <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/024.webp" class="w-full" />
+              <a href="#!">
+                <div
+                  class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                  style="background-color: rgba(251, 251, 251, 0.15)"></div>
+              </a>
+            </div>
+          </div>
+          <div class="p-6">
+            <h5 class="font-bold text-lg mb-3 text-amber-500">My paradise</h5>
+            <p class="text-gray-500 mb-4">
+              <small>Published <u>13.01.2022</u> by
+                <a href="" class="text-gray-900">Anna Maria Doe</a></small>
+            </p>
+            <p class="mb-4 pb-2">
+              well-designed, lush, and abundant with plants, flowers, and other natural elements that create a peaceful and serene environment.
+            </p>
+            
+          </div>
+        </div>
+      </div>
 
 
-      <section class="card">
-                    <div class="rounded-lg shadow-lg bg-white mx-2 mt-10">
-
-                        <img class="rounded-t-lg w-full h-60"
-                            src="https://mdbcdn.b-cdn.net/img/new/standard/city/031.webp" alt="" />
-
-                        <div class="p-6">
-                            
-                                <h5 class="text-2xl text-amber-500 font-bold mb-2">AVIDA TOWERS SUCAT</h5>
-                       
-
-                            <div
-                                class="text-lg font-thin my-4 flex items-center text-white rounded-xl py-1 bg-sky-700 px-3 mr-2">
-                                <i class="fa-solid fa-location-dot text-white mr-2"></i> San Dionisio, Parañaque
-                            </div>
-
-                            <div class="flex items-center gap-1 mb-2">
-                                <p
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    Condominium
-                                </p>
-                                <p
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    model</p>
-                            </div>
-
-                            <div class="flex items-center gap-1">
-                                <div
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <span>Php </span>
-                                    From ₱ 2,220,000 to ₱ 5,630,000
-                                </div>
-                            </div>
-
-                            <br />
-                            <button type="button"
-                                class="inline-block px-6 py-2.5 border-2 border-sky-600 text-sky-600
-                              hover:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-700 hover:shadow-lg focus:bg-sky-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-800 active:shadow-lg transition duration-150 ease-in-out">
-                                Button
-                            </button>
-
-                        </div>
-                    </div>
-                </section>
+      <div class="mb-6 lg:mb-0 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-10 duration-300">
+        <div class="relative block bg-white rounded-lg shadow-lg h-full">
+          <div class="flex">
+            <div
+              class="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+              data-mdb-ripple="true" data-mdb-ripple-color="light">
+              <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/031.webp" class="w-full" />
+              <a href="#!">
+                <div
+                  class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                  style="background-color: rgba(251, 251, 251, 0.15)"></div>
+              </a>
+            </div>
+          </div>
+          <div class="p-6">
+            <h5 class="font-bold text-lg mb-3 text-amber-500">Travel to Italy</h5>
+            <p class="text-gray-500 mb-4">
+              <small>Published <u>12.01.2022</u> by
+                <a href="" class="text-gray-900">Halley Frank</a></small>
+            </p>
+            <p class="mb-4 pb-2">
+              Travel to Italy can be a wonderful experience, offering beautiful scenery, rich history, delicious food, and a vibrant culture. Here are some tips to help you plan your trip to Italy:
+            </p>
+            
+          </div>
+        </div>
+      </div>
 
 
-      <section class="card">
-                    <div class="rounded-lg shadow-lg bg-white mx-2 mt-10">
+      <div class="mb-0 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-10 duration-300">
+        <div class="relative block bg-white rounded-lg shadow-lg h-full">
+          <div class="flex">
+            <div
+              class="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+              data-mdb-ripple="true" data-mdb-ripple-color="light">
+              <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/081.webp" class="w-full" />
+              <a href="#!">
+                <div
+                  class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                  style="background-color: rgba(251, 251, 251, 0.15)"></div>
+              </a>
+            </div>
+          </div>
+          <div class="p-6">
+            <h5 class="font-bold text-lg mb-3 text-amber-500">Chasing the sun</h5>
+            <p class="text-gray-500 mb-4">
+              <small>Published <u>10.01.2022</u> by
+                <a href="" class="text-gray-900">Joe Svan</a></small>
+            </p>
+            <p class="mb-4 pb-2">
+              Chasing the sun is a phrase often used to describe the act of seeking out warmer climates, usually during the winter months. Many people who live in colder regions of the world may travel to sunnier destinations to escape the cold and enjoy some warmth and sunshine.
 
-                        <img class="rounded-t-lg w-full h-60"
-                            src="https://mdbcdn.b-cdn.net/img/new/standard/city/081.webp" alt="" />
-
-                        <div class="p-6">
-                            
-                                <h5 class="text-2xl text-amber-500 font-bold mb-2">BELLEFORT ESTATES</h5>
-                            
-
-                            <div
-                                class="text-lg font-thin my-4 flex items-center text-white rounded-xl py-1 bg-sky-700 px-3 mr-2">
-                                <i class="fa-solid fa-location-dot text-white mr-2"></i> Molino IV, Bacoor
-                            </div>
-
-                            <div class="flex items-center gap-1 mb-2">
-                                <p
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    House
-                                </p>
-                                <p
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    model</p>
-                            </div>
-
-                            <div class="flex items-center gap-1">
-                                <div
-                                    class="flex items-center justify-center border-2 border-sky-600 text-black rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <span>Php </span>
-                                    From ₱ 6,492,803 to ₱ 8,838,900
-                                </div>
-                                <div>
-                                 
-                                </div>
-                            </div>
-
-                            <br />
-                            <button type="button"
-                                class="inline-block px-6 py-2.5 border-2 border-sky-600 text-sky-600
-                                              hover:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-700 hover:shadow-lg focus:bg-sky-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-800 active:shadow-lg transition duration-150 ease-in-out">
-                                Button
-                            </button>
-
-                        </div>
-                    </div>
-                </section>
+Some popular destinations for chasing the sun include tropical locations like Hawaii, the Caribbean, and Southeast Asia. These destinations offer warm weather year-round and are popular among those looking to escape the winter chill.
+            </p>
+            
+          </div>
+        </div>
+      </div>
     </div>
 
   </section>
